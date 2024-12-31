f723a4394331ef6e409f95a0c749648976983bf5
Steven Lynn <i@stv.lol>
2024-12-31 23:47:07 +0800

    update footer

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,25 @@ import GlobeBackground from './components/GlobeBackground.vue'
 </script>
 
 <template>
-  <GlobeBackground />
-  <CountdownTimer />
+  <div class="relative min-h-screen">
+    <GlobeBackground />
+    <CountdownTimer />
+    <footer class="fixed bottom-4 w-full flex justify-center items-center gap-4">
+      <a href="https://github.com/stvlynn/NewYear" target="_blank" rel="noopener noreferrer">
+        <img alt="GitHub" src="https://img.shields.io/github/stars/stvlynn/NewYear?style=social" />
+      </a>
+      <a href="https://kanji.twi.am" target="_blank" rel="noopener noreferrer">
+        <img alt="Test your yearly Kanji" src="https://img.shields.io/badge/Test_your_yearly_Kanji-black?logo=x&style=flat-square" />
+      </a>
+    </footer>
+  </div>
 </template>
 
 <style>
 body {
   margin: 0;
-  background-color: #000000;
+  padding: 0;
   overflow: hidden;
+  background-color: #000;
 }
 </style>
